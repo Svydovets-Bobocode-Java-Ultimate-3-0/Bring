9950647d1ac8ea593f75fa133b49f44bc6492416
ibolshak <i.bolshak@comparus.de>
2023-11-09 20:11:47 +0200

    BR-5: Create CI negative test errorprone

--- src/test/java/TestCodeAnalysis.java
+++ src/test/java/TestCodeAnalysis.java
@@ -7,14 +7,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TestCodeAnalysis {
-//    public class ShortSet {
-//        public static void main (String[] args) {
-//            Set<Short> s = new HashSet<>();
-//            for (short i = 0; i < 100; i++) {
-//                s.add(i);
-//                s.remove(i - 1);
-//            }
-//            System.out.println(s.size());
-//        }
-//    }
+    public class ShortSet {
+        public static void main (String[] args) {
+            Set<Short> s = new HashSet<>();
+            for (short i = 0; i < 100; i++) {
+                s.add(i);
+                s.remove(i - 1);
+            }
+            System.out.println(s.size());
+        }
+    }
 }
