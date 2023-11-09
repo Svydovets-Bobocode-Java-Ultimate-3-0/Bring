378627008c6070b3cc4a301e9405a578816d9e8d
ibolshak <i.bolshak@comparus.de>
2023-11-09 20:10:12 +0200

    BR-5: Create CI refactor + test errorprone

---
+++ src/test/java/TestCodeAnalysis.java
@@ -0,0 +1,20 @@
+/*
+ * This file is a subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestCodeAnalysis {
+//    public class ShortSet {
+//        public static void main (String[] args) {
+//            Set<Short> s = new HashSet<>();
+//            for (short i = 0; i < 100; i++) {
+//                s.add(i);
+//                s.remove(i - 1);
+//            }
+//            System.out.println(s.size());
+//        }
+//    }
+}
