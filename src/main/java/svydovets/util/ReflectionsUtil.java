9bc958d6c9d8d52fa04f5e35d55b2785e52e28da
AlexandrNavozenko <alexandr.navozenko.dev@gmail.com>
2023-11-09 18:55:55 +0100

    BR-12: Implement Bean Definition

--- src/main/java/svydovets/util/ReflectionsUtil.java
+++ src/main/java/svydovets/util/ReflectionsUtil.java
@@ -1,4 +1,15 @@
 package svydovets.util;
 
+import svydovets.core.context.beanDefenition.BeanDefinition;
+import svydovets.core.context.beanDefenition.DefaultBeanDefinition;
+
+
 public class ReflectionsUtil {
+
+    private BeanDefinition createBeanDefinitionByBeanClass(Class<?> classType) {
+        var beanDefinitionBuilder = DefaultBeanDefinition.builder().beanClass(classType);
+        
+        return beanDefinitionBuilder.build();
+    }
+
 }
