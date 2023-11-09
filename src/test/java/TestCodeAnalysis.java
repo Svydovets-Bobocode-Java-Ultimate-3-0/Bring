c728ffc4dd9e80fb9e8ee2234172b767285aa57f
ibolshak <i.bolshak@comparus.de>
2023-11-09 20:13:32 +0200

    BR-5: Create CI revert errorprone

--- src/test/java/TestCodeAnalysis.java
+++ src/test/java/TestCodeAnalysis.java
@@ -6,15 +6,16 @@
 import java.util.HashSet;
 import java.util.Set;
 
+//Test class for testing cron error
 public class TestCodeAnalysis {
-    public class ShortSet {
-        public static void main (String[] args) {
-            Set<Short> s = new HashSet<>();
-            for (short i = 0; i < 100; i++) {
-                s.add(i);
-                s.remove(i - 1);
-            }
-            System.out.println(s.size());
-        }
-    }
+//    public class ShortSet {
+//        public static void main (String[] args) {
+//            Set<Short> s = new HashSet<>();
+//            for (short i = 0; i < 100; i++) {
+//                s.add(i);
+//                s.remove(i - 1);
+//            }
+//            System.out.println(s.size());
+//        }
+//    }
 }
