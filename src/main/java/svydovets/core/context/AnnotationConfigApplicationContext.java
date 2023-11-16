ef59da6ad19a0e6d2786b013e5e8156f84b36ece
Safarov Renat <SafarovRenat1995@gmail.com>
2023-11-16 12:41:16 +0200

    BR-23 IoC container architecture has been refactored 1) DefaultApplicationContext class renamed to AnnotationConfigApplicationContext 2) Logic of "DefaultApplicationContext" moved to new classes: * BeanFactory * BeanDefinitionFactory

---
+++ src/main/java/svydovets/core/context/AnnotationConfigApplicationContext.java
@@ -0,0 +1,32 @@
+package svydovets.core.context;
+
+import svydovets.core.context.beanFactory.BeanFactory;
+
+import java.util.Map;
+
+public class AnnotationConfigApplicationContext implements ApplicationContext {
+    private final BeanFactory beanFactory = new BeanFactory();
+
+    public AnnotationConfigApplicationContext(String basePackage) {
+        beanFactory.registerBeans(basePackage);
+    }
+
+    public AnnotationConfigApplicationContext(Class<?>... classes) { // @Config or @Component
+        beanFactory.registerBeans(classes);
+    }
+
+    @Override
+    public <T> T getBean(Class<T> requiredType) {
+        return beanFactory.getBean(requiredType);
+    }
+
+    @Override
+    public <T> T getBean(String name, Class<T> requiredType) {
+        return beanFactory.getBean(name, requiredType);
+    }
+
+    @Override
+    public <T> Map<String, T> getBeansOfType(Class<T> requiredType) {
+        return beanFactory.getBeansOfType(requiredType);
+    }
+}
--- src/test/java/com/bobocode/svydovets/core/PostConstructTest.java
+++ src/test/java/com/bobocode/svydovets/core/PostConstructTest.java
@@ -4,8 +4,8 @@ import com.bobocode.svydovets.service.postconstruct.valid.PostConstructService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import svydovets.core.context.AnnotationConfigApplicationContext;
 import svydovets.core.context.ApplicationContext;
-import svydovets.core.context.DefaultApplicationContext;
 import svydovets.exception.NoUniquePostConstructException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
@@ -15,7 +15,7 @@ public class PostConstructTest {
 
     @Test
     void shouldInvokeThePostConstructMethodAfterBeanInitialization() {
-        ApplicationContext context = new DefaultApplicationContext("com.bobocode.svydovets.service.postconstruct.valid");
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.bobocode.svydovets.service.postconstruct.valid");
         PostConstructService postConstructService = context.getBean(PostConstructService.class);
         Assertions.assertThat(postConstructService.getName()).isEqualTo("post-construct-service");
     }
@@ -23,7 +23,7 @@ public class PostConstructTest {
     @Test
     void shouldThrowExceptionWhenServiceHasTwoPostConstructAnnotations() {
         assertThatExceptionOfType(NoUniquePostConstructException.class)
-                .isThrownBy(() -> new DefaultApplicationContext("com.bobocode.svydovets.service.postconstruct.invalid"))
+                .isThrownBy(() -> new AnnotationConfigApplicationContext("com.bobocode.svydovets.service.postconstruct.invalid"))
                 .withMessage("You cannot have more than one method that is annotated with @PostConstruct.");
     }
 
