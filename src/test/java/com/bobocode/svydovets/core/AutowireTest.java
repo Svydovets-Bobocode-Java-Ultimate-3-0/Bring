f200828f27023b3a9977bd463f493da9ada64a5e
ihor <i.zaporozhtsev@gmail.com>
2023-11-16 20:13:12 +0200

    BR-19 add setter autowire injection

--- src/test/java/com/bobocode/svydovets/core/AutowireTest.java
+++ src/test/java/com/bobocode/svydovets/core/AutowireTest.java
@@ -2,6 +2,7 @@ package com.bobocode.svydovets.core;
 
 import com.bobocode.svydovets.service.base.EditService;
 import com.bobocode.svydovets.service.base.OrderService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import svydovets.core.context.AnnotationConfigApplicationContext;
 import svydovets.core.context.ApplicationContext;
@@ -9,23 +10,28 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AutowireTest {
 
+    private final static String BASE_PACKAGE = "com.bobocode.svydovets.service.base";
+    private ApplicationContext context;
+
+    @BeforeEach
+    public void setUp(){
+        context = new AnnotationConfigApplicationContext(BASE_PACKAGE);
+    }
+
     @Test
     void shouldNotNullWhenFieldIsInjected(){
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.bobocode.svydovets.service.base");
         EditService editService = context.getBean(EditService.class);
         assertThat(editService.getMessageService()).isNotNull();
     }
 
     @Test
     void shouldReturnNullIfFieldNotInjected(){
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.bobocode.svydovets.service.base");
         EditService editService = context.getBean(EditService.class);
         assertThat(editService.getCommonService()).isNull();
     }
 
     @Test
     void shouldNotNullWhenFieldIsInjectedViaSetter(){
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.bobocode.svydovets.service.base");
         OrderService orderService = context.getBean(OrderService.class);
         assertThat(orderService.getMessageService()).isNotNull();
         assertThat(orderService.getCommonService()).isNotNull();
@@ -34,7 +40,6 @@ public class AutowireTest {
 
     @Test
     void shouldBeNullWhenFieldIsInjectedViaSetter(){
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.bobocode.svydovets.service.base");
         OrderService orderService = context.getBean(OrderService.class);
         assertThat(orderService.getNullService()).isNull();
     }
