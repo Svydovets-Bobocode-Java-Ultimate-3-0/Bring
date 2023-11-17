d4fe23dc2e47424ea18931c90e647e326c497fc3
Safarov Renat <SafarovRenat1995@gmail.com>
2023-11-17 14:06:49 +0200

    BR-25 Added @Component for new classes(implementations): * "FirstNotPrimaryPaymentServiceImpl" * "SecondNotPrimaryPaymentServiceImpl"

--- src/test/java/com/bobocode/svydovets/source/qualifier/withoutPrimary/FirstNotPrimaryPaymentServiceImpl.java
+++ src/test/java/com/bobocode/svydovets/source/qualifier/withoutPrimary/FirstNotPrimaryPaymentServiceImpl.java
@@ -1,4 +1,7 @@
 package com.bobocode.svydovets.source.qualifier.withoutPrimary;
 
+import svydovets.core.annotation.Component;
+
+@Component
 public class FirstNotPrimaryPaymentServiceImpl implements PaymentService {
 }
