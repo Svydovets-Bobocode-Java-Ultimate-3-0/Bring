b63912bf0a2069163119f18b42d44be12f701765
Oleksii Makieiev <alexmakeev2703@gmail.com>
2023-11-21 21:24:18 +0200

    Add JavaDoc

--- src/main/java/svydovets/exception/NoMatchingPatternFoundException.java
+++ src/main/java/svydovets/exception/NoMatchingPatternFoundException.java
@@ -1,5 +1,9 @@
 package svydovets.exception;
 
+/**
+ * Exception thrown when no matching pattern is found for a given request path
+ * within a set of predefined pattern paths.
+ */
 public class NoMatchingPatternFoundException extends RuntimeException {
 
     public NoMatchingPatternFoundException(String message) {
