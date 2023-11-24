90e8cf2a8e92dcdf5dda411a5f42ec08b37e5291
Safarov Renat <SafarovRenat1995@gmail.com>
2023-11-24 21:09:43 +0200

    BR-Web-45 Code improvement

--- src/main/java/svydovets/web/DispatcherServlet.java
+++ src/main/java/svydovets/web/DispatcherServlet.java
@@ -3,8 +3,6 @@ package svydovets.web;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,7 +15,6 @@ import svydovets.web.path.RequestInfoCreator;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class DispatcherServlet extends HttpServlet {
 
@@ -38,28 +35,17 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
-
-    }
-
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        super.service(req, res);
-    }
-
-
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            processRequest(req, MethodNameEnum.GET);
+            Object result = processRequest(req, MethodNameEnum.GET);
         } catch (Exception e) {
-            //todo: if this value is null, throws exception
+
         }
+        // todo: Process "result"
     }
 
     // todo: Remove "throws Exception"
-    private void processRequest(HttpServletRequest req, MethodNameEnum httpMethodName) throws Exception {
+    private Object processRequest(HttpServletRequest req, MethodNameEnum httpMethodName) throws Exception {
         String requestPath = req.getPathInfo();
 
         RequestInfoHolder requestInfoHolder = webApplicationContext.getRequestInfoHolder(httpMethodName, requestPath);
@@ -74,51 +60,36 @@ public class DispatcherServlet extends HttpServlet {
 
         RequestInfo requestInfo = RequestInfoCreator.create(req);
         Object[] requestArguments = webInvocationHandler.invoke(methodToInvoke, requestInfo);
-        methodToInvoke.invoke(controller, requestArguments);
+        return methodToInvoke.invoke(controller, requestArguments);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Object result = processRequest(req, MethodNameEnum.POST);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Object result = processRequest(req, MethodNameEnum.PUT);
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Object result = processRequest(req, MethodNameEnum.DELETE);
+        } catch (Exception e) {
+        }
     }
 
     private String getControllerMethodPath(String requestPath, MethodNameEnum httpMethodName) {
         Set<String> patternPath = webApplicationContext.getMethodPatterns(httpMethodName);
         return pathFinder.find(requestPath, patternPath);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Set<String> patternPaths = webApplicationContext.getMethodPatterns(MethodNameEnum.POST);
-
-        String foundPattern = pathFinder.find(req.getPathInfo(), patternPaths);
-
-        RequestInfoHolder requestInfoHolder = webApplicationContext.getRequestInfoHolder(MethodNameEnum.POST, foundPattern);
-        //todo: if this value is null, throws exception
-    }
-
-    @Override
-    protected long getLastModified(HttpServletRequest req) {
-        return super.getLastModified(req);
-    }
-
-    @Override
-    protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doHead(req, resp);
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
-    }
-
-    @Override
-    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doOptions(req, resp);
-    }
-
-    @Override
-    protected void doTrace(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doTrace(req, resp);
     }
 }
