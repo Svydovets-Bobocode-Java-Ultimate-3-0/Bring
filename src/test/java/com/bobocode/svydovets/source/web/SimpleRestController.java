d1400931f57385d8126be2ef317eb72d4c9c0c58
Oleksandr <laskovskiu2017@gmail.com>
2023-11-30 21:14:52 +0200

    Add tests for resolveRequestParameterName method

--- src/test/java/com/bobocode/svydovets/source/web/SimpleRestController.java
+++ src/test/java/com/bobocode/svydovets/source/web/SimpleRestController.java
@@ -3,6 +3,7 @@ package com.bobocode.svydovets.source.web;
 import svydovets.web.annotation.GetMapping;
 import svydovets.web.annotation.PathVariable;
 import svydovets.web.annotation.RequestMapping;
+import svydovets.web.annotation.RequestParam;
 import svydovets.web.annotation.RestController;
 
 @RestController("simpleContr")
@@ -10,8 +11,23 @@ import svydovets.web.annotation.RestController;
 public class SimpleRestController {
 
     @GetMapping("/hello/{name}")
-    public void hello(@PathVariable String name) {
+    public void helloPath(@PathVariable String name) {
     System.out.println(name);
+    }
+
+    @GetMapping("/goodbye/{name}")
+    public void goodbyePath(@PathVariable("bye") String goodbye) {
+        System.out.println(goodbye);
+    }
+
+    @GetMapping("/hello")
+    public void helloRequest(@RequestParam String name) {
+        System.out.println(name);
+    }
+
+    @GetMapping("/goodbye")
+    public void goodbyeRequest(@RequestParam("bye") String goodbye) {
+        System.out.println(goodbye);
     }
 
 }
