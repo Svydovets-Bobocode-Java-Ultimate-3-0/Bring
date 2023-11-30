84fc093ada85ed27e7e8f8d956a1b41eac8a5943
Safarov Renat <SafarovRenat1995@gmail.com>
2023-12-01 01:25:51 +0200

    BR-53 Add documentation for "@Component", "@ComponentScan", "@Configuration"

--- src/main/java/svydovets/core/annotation/Component.java
+++ src/main/java/svydovets/core/annotation/Component.java
@@ -5,6 +5,32 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The @Component annotation is used to indicate that a class is component of context.
+ * It is a generic annotation that designates a class as bean, allowing IoC
+ * container to manage its lifecycle. Components are typically Java classes that encapsulate business logic or other
+ * processing capabilities within a user application.
+ *
+ * <h2>Purpose</h2>
+ * <ul>
+ *     <li>The primary purpose of the @Component annotation is to identify a class as bean or component.
+ *     It allows the class to be automatically detected and registered during the component scanning process.</li>
+ * </ul>
+ *
+ * <h2>Bean Naming</h2>
+ * <p>By default, the bean name is generated based on the class name with the initial lowercase letter (e.g., a class named MyComponent becomes the bean with the name myComponent).
+ * Custom bean names can be specified using the value attribute of the @Component annotation.</p>
+ *
+ * <pre class="code">
+ * &#064;Component
+ * public class MyComponentWithDefaultName {
+ * }
+ *
+ * &#064;Component("customName")
+ * public class MyComponentWithCustomName {
+ * }
+ * </pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Component {
--- src/main/java/svydovets/core/annotation/Configuration.java
+++ src/main/java/svydovets/core/annotation/Configuration.java
@@ -6,9 +6,91 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * The @Configuration annotation is used to indicate that a class declares one or more @Bean methods, and its purpose
- * is to define user's custom beans. . The @Configuration annotation is an integral part of the
- * Java-based configuration approach in Spring, providing an alternative to XML-based configuration.
+ * The @Configuration annotation is used to indicate that a class declares one or more @Bean methods, and define user's custom beans. The @Configuration annotation is an integral part of the creating application context.
+ *
+ * <h2>Purpose</h2>
+ * <ul>
+ *     <li>The primary purpose of the @Configuration annotation is to define Java-based configuration classes</li>
+ *     <li>It is used to mark a class as a configuration class, indicating that it contains bean definitions.</li>
+ * </ul>
+ *
+ * <h2>How It Works</h2>
+ * <p>
+ *     During the application startup, the Spring IoC container scans the classpath for classes annotated with {@code @Configuration}.
+ *     For each {@code @Configuration} class found, the container processes {@code @Bean} methods within that class.
+ *     {@code @Bean} methods are invoked, and the objects they return are registered as Spring beans.
+ *     Dependencies between beans are resolved, and the Spring IoC container manages the lifecycle of these beans.
+ * </p>
+ *
+ * <h2>Bean Declaration</h2>
+ * <ul>
+ *     <li>Within a class annotated with @Configuration, you can declare bean definitions using methods annotated with @Bean</li>
+ *     <li>These @Bean methods return instances of the objects that should be managed by the Bring IoC container.</li>
+ * </ul>
+ * <pre class="code">
+ * &#064;Configuration
+ * public class TestConfig {
+ *     &#064;Bean
+ *     public MessageService messageService() {
+ *         MessageService messageService = new MessageService();
+ *         messageService.setMessage("Hello from \"MessageService\"");
+ *         return messageService;
+ *     }
+ * }
+ * </pre>
+ *
+ * <h2>Context Configuration</h2>
+ * <ul>
+ *     <li>Annotated classes are typically used in conjunction with the AnnotationConfigApplicationContext or
+ *     AnnotationConfigWebApplicationContext to load the context based on Java configuration. For examole:</li>
+ * </ul>
+ *
+ * <pre class="code">
+ * AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+ * </pre>
+ *
+ * <h2>Component Scanning</h2>
+ * <ul>
+ *     <li>In addition to manually declaring beans using @Bean methods, @Configuration classes often leverage component scanning.
+ *     Component scanning automatically detects and registers components (beans) in specified packages. For example:</li>
+ * </ul>
+ * <pre class="code">
+ * &#064;Configuration
+ * &#064;ComponentScan(com.example.beans)
+ * public class TestConfig {
+ *     &#064;Bean
+ *     public MessageService messageService() {
+ *         MessageService messageService = new MessageService();
+ *         messageService.setMessage("Hello from \"MessageService\"");
+ *         return messageService;
+ *     }
+ * }
+ * </pre>
+ *
+ * <h2>Bean Dependencies</h2>
+ * <ul>
+ *     <li>@Configuration classes can express dependencies between beans, ensuring that beans are created in the
+ *     correct order and with the required dependencies. For example:</li>
+ * </ul>
+ * <pre class="code">
+ * &#064;Configuration
+ * public class TestConfig {
+ *     &#064;Bean
+ *     public MessageService messageService() {
+ *         MessageService messageService = new MessageService();
+ *         messageService.setMessage("Hello from \"MessageService\"");
+ *         return messageService;
+ *     }
+ *     &#064;Bean
+ *     public PrintService printService(MessageService messageService) {
+ *         return new PrintService(messageService);
+ *     }
+ * }
+ * </pre>
+ *
+ * @see Bean
+ * @see ComponentScan
+ * @see svydovets.core.context.ApplicationContext
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
