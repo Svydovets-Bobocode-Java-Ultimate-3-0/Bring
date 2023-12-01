4ddda5c499cf5ef12f9cf10fcb7c711957018fe4
ibolshak <i.bolshak@comparus.de>
2023-12-02 00:33:19 +0200

    added docs for crud web-annotation

--- src/main/java/svydovets/web/annotation/DeleteMapping.java
+++ src/main/java/svydovets/web/annotation/DeleteMapping.java
@@ -5,6 +5,26 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The {@code DeleteMapping} annotation is used to map HTTP DELETE requests onto specific handler methods.
+ * This annotation is specifically targeted to be used on methods.
+ *
+ * <p>Typically, {@code DeleteMapping} is used to mark a method in a controller that should handle a
+ * DELETE request to a specified URI. The {@code value} attribute of the annotation defines this URI.
+ *
+ * <p>Usage example:
+ * <pre>
+ * {@code
+ *     @DeleteMapping("/{id}")
+ *     public void removeUser(@PathVariable Long id) {
+ *         // Method implementation
+ *     }
+ * }
+ * </pre>
+ *
+ * @author @Safarov Renat, Oleksii Makieiev
+ * @version 1.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DeleteMapping {
--- src/main/java/svydovets/web/annotation/GetMapping.java
+++ src/main/java/svydovets/web/annotation/GetMapping.java
@@ -5,6 +5,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The {@code GetMapping} annotation is used for mapping HTTP GET requests to specific handler methods.
+ * This annotation is intended for use on methods within a web controller class.
+ *
+ * <p>{@code GetMapping} is typically applied to a public method in a controller to denote that it should
+ * handle a GET request for a specified URI. The {@code value} attribute of the annotation defines this URI.
+ * When an HTTP GET request is made to the specified URI, the annotated method is invoked to process the request.
+ *
+ * <p>Usage example:
+ * <pre>
+ * {@code
+ *   @GetMapping("/users/{id}")
+ *   public User getUser(@PathVariable Long id) {
+ *       // Method implementation to return a user
+ *   }
+ * }
+ * </pre>
+ *
+ * <p>Note: This annotation assumes the presence of a framework capable of processing HTTP requests and
+ * routing them to methods annotated with {@code GetMapping}, similar to how the Spring Framework operates.
+ *
+ * @author @Renat Safarov
+ * @version 1.0
+ * @see PathVariable
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface GetMapping {
