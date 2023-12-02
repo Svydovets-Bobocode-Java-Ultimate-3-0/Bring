83a47de9c34a65df3dc500b3655063b417577cce
Safarov Renat <SafarovRenat1995@gmail.com>
2023-12-02 09:21:47 +0200

    Removed unused classes, methods

--- src/main/java/svydovets/web/exception/TomcatStartingException.java
+++ src/main/java/svydovets/web/exception/TomcatStartingException.java
@@ -2,9 +2,6 @@ package svydovets.web.exception;
 
 public class TomcatStartingException extends RuntimeException {
 
-    public TomcatStartingException(String message) {
-        super(message);
-    }
 
     public TomcatStartingException(String message, Throwable cause) {
         super(message, cause);
