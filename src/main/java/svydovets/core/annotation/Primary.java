6d13c30feeaa5c08da8a0c15b0b8a6216d6be42b
Safarov Renat <SafarovRenat1995@gmail.com>
2023-12-02 02:17:11 +0200

    Last changes from Oleksandr

--- src/main/java/svydovets/core/annotation/Primary.java
+++ src/main/java/svydovets/core/annotation/Primary.java
@@ -5,6 +5,32 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <p>
+ *     Primary annotation is used to give higher preference to bean when there are multiple beans of the same type.
+ *     It will use bean marked with @Primary when single candidate is required for autowiring.
+ * </p>
+ * <p>
+ *     When class annotated with @Primary, it should prefer this bean when multiple candidates
+ *     are qualified to autowire a single-valued dependency. If @Primary is not present,
+ *     will choose bean to autowire by type or name, and if there's still ambiguity,
+ *     it will throw a NoUniqueBeanDefinitionException.
+ * </p>
+ * <p>
+ * Here is example of how to use the Primary annotation:
+ * </p>
+ * <pre class="code">
+ * &#064;Primary
+ * &#064;Component
+ * public class PrimaryInjectionCandidate implements InjectionCandidate {
+ * }
+ * </pre>
+ * <p>
+ *     In this example, PrimaryInjectionCandidate class is annotated with @Primary.
+ *     This means that when it needs to autowire bean of type InjectionCandidate,
+ *     it will prefer to use instance of PrimaryInjectionCandidate, if available.
+ * </p>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Primary {
--- src/main/java/svydovets/web/annotation/RequestMapping.java
+++ src/main/java/svydovets/web/annotation/RequestMapping.java
@@ -5,6 +5,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <p>
+ *     RequestMapping annotation is used to map web requests onto specific handler classes and/or handler methods.
+ *     It provides routing information and tells which URI matches which method.
+ * </p>
+ * <p>
+ *     When class or a method annotated with @RequestMapping, this class or method should handle requests to certain URI.
+ *     The value attribute of RequestMapping annotation specifies URI pattern that class or method will handle.
+ * </p>
+ * <p>
+ *     Here is example of how to use the RequestMapping annotation:
+ * </p>
+ * <pre class="code">
+ * &#064;RestController
+ * &#064;RequestMapping("/users")
+ * public class UserController {
+ *     // handler methods
+ * }
+ * </pre>
+ * <p>
+ *     In this example, UserController class is annotated with @RequestMapping("/users").
+ *     This means that this class will handle all requests that start with "/users".
+ *     For example, GET request to "/users" might return list of users, while POST request to "/users" might create new user.
+ * </p>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface RequestMapping {
